83719942cc8ef3d491debb526c6c274f1d3585ce
sonny parsonage <sonnyparsonage@gmail.com>
2016-09-13 19:23:36 +0100

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+	context: __dirname,
+    entry: "./app/app.jsx",
+    output: {
+    	path: path.join(__dirname,'/public/js'),
+        filename: "bundle.js",
+        sourceMapFilename: "public/js/bundle.map"
+    },
+    resolve: {
+    	extensions: ['','.js','.jsx','.json']
+    },
+    devtool: '#source-map',
+    module: {
+    	preLoaders: [
+    	{
+    		test: /\.jsx?$/,
+        	loader: 'eslint-loader'
+    	}
+    	],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, '/app')
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+   }
+  }
