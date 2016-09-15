86daae8a296745721f02a1caac26e551ade78810
sonny parsonage <sonnyparsonage@gmail.com>
2016-09-15 11:19:12 +0100

    root component is now react router

--- app/app.jsx
+++ app/app.jsx
@@ -8,11 +8,11 @@ ReactDOM.render(Router, document.getElementById('root'))
 const React = require('react')
 const ReactRouter = require('react-router')
 const ReactDOM = require('react-dom')
-const App = require('./router')
+
 
 
 const MainLayout = require('./components/MainLayout')
-const Routing = require('./components/Home')
+const Routing = require('./router')
 
 ReactDOM.render(<Routing/>, document.getElementById('root'));
 
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const Navbar = React.createClass({
+  render: function(){
+    return (
+        <div>
+          <h1>Hello world</h1>
+        </div>
+        )
+  }
+})
+  
+module.exports = Navbar
